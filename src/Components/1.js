c5a8ca0292cb3f84ddee7483091d99dc624d4b78
sayandeeppaul-anotech <sayandeep.paul@anotechsolutions.com>
2024-05-18 16:19:46 +0530

    new update

--- src/Components/1.js
+++ src/Components/1.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function 1() {
+function Sayandeep() {
   return (
     <div>
-      <h1>123</h1>
+      <h1>Sayandeep</h1>
     </div>
   )
 }
 
-export default 1
+export default Sayandeep
